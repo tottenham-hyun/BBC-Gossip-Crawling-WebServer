d471f3b41a42ded8e51c652b1bdb2a7d1fbd08e5
tottenham-hyun <hwk3988@gmail.com>
2025-01-16 02:31:09 +0900

    add modify-info

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -6,9 +6,13 @@ const router = Router()
 router.post('/signup', checkNotAuthenticated ,userController.userSignup)
 router.post('/login', checkNotAuthenticated, userController.userLogin)
 router.post('/logout', checkAuthenticated, userController.userLogout)
-router.put('/subscribe', checkAuthenticated, userController.userSubscribe)
-router.put('/unsubscribe', checkAuthenticated, userController.userUnsubscribe)
-router.post('/:id/send-code', checkAuthenticated, userController.sendWithdrawMail)
+
+router.patch('/subscribe', checkAuthenticated, userController.userSubscribe)
+router.patch('/unsubscribe', checkAuthenticated, userController.userUnsubscribe)
+
+router.put('/:id/modify-info', checkAuthenticated, userController.userModifyInfo)
+
+router.post('/:id/send-code', checkAuthenticated, userController.sendCode)
 router.delete('/:id/withdraw', checkAuthenticated, userController.userWithdraw)
 
 export default router
