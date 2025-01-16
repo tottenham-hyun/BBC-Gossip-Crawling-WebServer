de08f2e4ac610cb8ccd3a09a43f71d80decbeb00
tottenham-hyun <hwk3988@gmail.com>
2025-01-16 22:32:56 +0900

    비밀번호 변경 라우팅 추가

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -10,7 +10,8 @@ router.post('/logout', checkAuthenticated, userController.userLogout)
 router.patch('/subscribe', checkAuthenticated, userController.userSubscribe)
 router.patch('/unsubscribe', checkAuthenticated, userController.userUnsubscribe)
 
-router.put('/:id/modify-info', checkAuthenticated, userController.userModifyInfo)
+router.patch('/:id/modify-info', checkAuthenticated, userController.userModifyInfo)
+router.patch('/:id/modify-password', checkAuthenticated, userController.userChangePassword)
 
 router.post('/:id/send-code', checkAuthenticated, userController.sendCode)
 router.delete('/:id/withdraw', checkAuthenticated, userController.userWithdraw)
